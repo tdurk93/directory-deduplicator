01589c76c7dad7d0d74e72b356dc6df89f1a37a5
Tyler Durkota <tdurk93@gmail.com>
2021-05-04 17:44:16 -0700

    linting fixes

--- directorydeduplicator.py
+++ directorydeduplicator.py
@@ -12,19 +12,22 @@ import sys
 import xxhash
 import zlib
 
-BUFFER_SIZE = 1024*1024*10 # 10MB
+BUFFER_SIZE = 1024 * 1024 * 10  # 10MB
 
 
 def build_tree(
-        directory_path: str, parent: Optional[DirectoryNode],
-        directory_hash_map: Dict[str, List[DirectoryNode]],
-        safe_hash: bool = False,
-        follow_symlinks: bool = False) -> Tuple[DirectoryNode, Dict[str, List[DirectoryNode]]]:
+    directory_path: str,
+    parent: Optional[DirectoryNode],
+    directory_hash_map: Dict[str, List[DirectoryNode]],
+    safe_hash: bool = False,
+    follow_symlinks: bool = False
+) -> Tuple[DirectoryNode, Dict[str, List[DirectoryNode]]]:
     node = DirectoryNode(path=directory_path, parent=parent)
     try:
         entries = os.scandir(directory_path)
     except PermissionError:
-        print(f"Could not open directory {directory_path}: permission denied", file=sys.stderr)
+        print(f"Could not open directory {directory_path}: permission denied",
+              file=sys.stderr)
         return node, directory_hash_map
     for entry in sorted(entries, key=lambda x: x.name):
         if entry.is_file() and (follow_symlinks or not entry.is_symlink()):
@@ -40,7 +43,8 @@ def build_tree(
                         hash_object.update(file_chunk)
                         running_crc32 = zlib.crc32(file_chunk, running_crc32)
             except PermissionError:
-                print(f"Could not open file {entry.path}: permission denied", file=sys.stderr)
+                print(f"Could not open file {entry.path}: permission denied",
+                      file=sys.stderr)
                 # use file name & size as stand-in for file contents
                 digest = f"PERMISSION_DENIED: {file_size} {entry.path}"
                 hash_object.update(digest)
@@ -51,7 +55,7 @@ def build_tree(
             if (file_size == 0):
                 file_hash = "EMPTY"  # override prev value, if applicable
             node.files[entry.path] = FileNode(file_size, file_hash)
-        elif entry.is_dir() and not entry.is_symlink(): # TODO add symlink support?
+        elif entry.is_dir() and not entry.is_symlink():
             node.subdirectory_nodes[
                 entry.path], subdirectory_hash_map = build_tree(
                     entry.path, node, directory_hash_map, safe_hash)
@@ -60,7 +64,8 @@ def build_tree(
         # are already duplicated in list. Remove immediate children nodes
         # from hash map, so we only track the roots of duplicate subtrees
         for child_dir_node in node.subdirectory_nodes.values():
-            directory_hash_map[child_dir_node.get_hash()].remove(child_dir_node)
+            directory_hash_map[child_dir_node.get_hash()].remove(
+                child_dir_node)
     directory_hash_map[node.get_hash()].append(node)
     return node, directory_hash_map
 
@@ -112,26 +117,29 @@ def bytes2human(n: int, format: str = "%(value)i%(symbol)s") -> str:
               help="Follow symbolic links")
 def run(safe_hash: bool, follow_symlinks: bool, directory_path: str) -> None:
     root_node, directory_hash_map = build_tree(directory_path,
-                                           None,
-                                           defaultdict(list),
-                                           safe_hash=safe_hash,
-                                           follow_symlinks=follow_symlinks)
+                                               None,
+                                               defaultdict(list),
+                                               safe_hash=safe_hash,
+                                               follow_symlinks=follow_symlinks)
 
     # add one to num_subdirectories for root node
     print(f"Scanned {root_node.get_num_subdirectories() + 1} directories " +
           f"({bytes2human(root_node.disk_space)})",
           file=sys.stderr)
 
-    duplicate_directory_sets = find_duplicate_directory_sets(directory_hash_map)
+    duplicate_directory_sets = find_duplicate_directory_sets(
+        directory_hash_map)
     potential_space_savings = 0
-    for directory_set in sorted(duplicate_directory_sets, key=lambda x: x.disk_space, reverse=True):
+    for dir_set in sorted(duplicate_directory_sets,
+                          key=lambda x: x.disk_space,
+                          reverse=True):
         summary = ", ".join([
-            f"{directory_set.num_files} files",
-            f"{directory_set.num_subdirectories} folders",
-            f"{bytes2human(directory_set.disk_space)}"
+            f"{dir_set.num_files} files",
+            f"{dir_set.num_subdirectories} folders",
+            f"{bytes2human(dir_set.disk_space)}"
         ])
-        print(f"Duplicate directory set ({summary}):")
-        print("\t" +
-              "\n\t".join([node.path for node in directory_set.directory_nodes]))
-        potential_space_savings += directory_set.disk_space * (len(directory_set.directory_nodes) - 1)
+        print(f"Duplicate directory set ({summary}):" +
+              "\n\t".join([node.path for node in dir_set.directory_nodes]))
+        potential_space_savings += dir_set.disk_space * (
+            len(dir_set.directory_nodes) - 1)
     print(f"Potential space savings: {bytes2human(potential_space_savings)}")

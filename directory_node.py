9dac22ae28aaaf15c647d67518b451411f8a10d0
Tyler Durkota <tdurk93@gmail.com>
2023-09-21 08:21:44 -0700

    Rewrite/refactor processing logic
    This should be much easier to understand
    (especially for me) and enable additional
    features moving forward.

--- directory_node.py
+++ directory_node.py
@@ -1,77 +1,28 @@
 from __future__ import annotations
 from typing import Any, cast, Dict, ValuesView
 from collections import OrderedDict
-import xxhash
-import zlib
+from os.path import basename
 
 
-class FileNode:
-    def __init__(self, disk_space: int, hash: str):
+class MiNode:  # represents a file or directory (play on "inode" and "MyNode")
+    def __init__(self, path: str, disk_space: int):
+        self.path = path
+        self.name = basename(path)  # returns empty string for directories
         self.disk_space = disk_space
-        self.hash = hash
+        self.hash = "" # TODO consider removing?
+
 
     def __repr__(self) -> str:
         return f"[hash: {self.hash}, disk_space: {self.disk_space}]"
 
 
-class DirectoryNode(FileNode):
-    def __init__(self, path: str):
-        super().__init__(disk_space=-1, hash="")
-        self.path = path
-        self.subdirectory_nodes: Dict[str, DirectoryNode] = OrderedDict()
-        self.files: Dict[str, FileNode] = OrderedDict()
-        self.num_files = -1  # includes files in subfolders
-        self.num_subdirectories = -1  # includes folders of subfolders
-
-    def get_hash(self) -> str:
-        if not self.hash:
-            self.__calculate_aggregate_values__()
-        return self.hash
-
-    def get_disk_usage(self) -> int:
-        if self.disk_space == -1:
-            self.__calculate_aggregate_values__()
-        return self.disk_space
-
-    def get_num_files(self) -> int:
-        if self.num_files == -1:
-            self.__calculate_aggregate_values__()
-        return self.num_files
-
-    def get_num_subdirectories(self) -> int:
-        if self.num_subdirectories == -1:
-            self.__calculate_aggregate_values__()
-        return self.num_subdirectories
-
-    def __calculate_aggregate_values__(self) -> Dict[str, Any]:
-        """
-        This has a side effect of saving to instance variables,
-        which is needed so that calculations for all child nodes are cached
-        """
-        subfolder_hash_tuple, subfolder_disk_usage_tuple, \
-            subfolder_num_file_tuple, subfolder_num_subdir_tuple = \
-            tuple(zip(*[child.__calculate_aggregate_values__().values() for child
-                        in self.subdirectory_nodes.values()]
-                      or cast(ValuesView[Any], [("", 0, 0, 0)])))
-        # need stable order for hashes to maintain file-renaming invariance
-        subfolder_hashes = "".join(sorted(subfolder_hash_tuple))
-        file_hashes = "".join(
-            sorted([file.hash for file in self.files.values()]))
-        file_and_folder_hashes = (subfolder_hashes + file_hashes).encode("utf-8")
-        self.hash = xxhash.xxh3_128_hexdigest(file_and_folder_hashes)
-        # concatenate second hash/checksum for reduced chance of collisions
-        self.hash += str(zlib.crc32(file_and_folder_hashes))
-        file_disk_space = sum([file.disk_space for file in self.files.values()])
-        self.disk_space = sum(subfolder_disk_usage_tuple, file_disk_space)
-        self.num_files = sum(subfolder_num_file_tuple, len(self.files))
-        self.num_subdirectories = sum(subfolder_num_subdir_tuple,
-                                     len(self.subdirectory_nodes))
-        return {
-            "hash": self.hash,
-            "disk_space": self.disk_space,
-            "num_files": self.num_files,
-            "num_subdirectories": self.num_subdirectories
-        }
+class DirectoryNode(MiNode):
+    def __init__(self, path: str, disk_space=0):
+        super().__init__(path=path, disk_space=disk_space)
+        self.subdir_nodes: Dict[str, DirectoryNode] = OrderedDict()
+        self.files: Dict[str, MiNode] = OrderedDict()
+        self.num_files = 0  # includes files in subfolders
+        self.num_subdirectories = 0  # includes folders of subfolders
 
     def __repr__(self) -> str:
         repr_lines = [f"{self.path}:\n  fileHashes for files:",
@@ -80,5 +31,5 @@ class DirectoryNode(FileNode):
                      " subdirectories:"] + \
             ["\n".join(
                 [f"\t{line}" for line in child.__repr__().split("\n")]
-            ) for child in self.subdirectory_nodes.values()] or ["none"]
+            ) for child in self.subdir_nodes.values()] or ["none"]
         return "\n".join(repr_lines)
--- directorydeduplicator.py
+++ directorydeduplicator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from directory_node import DirectoryNode, FileNode
+from directory_node import DirectoryNode, MiNode
 from duplicate_directory_set import DuplicateDirectorySet
 from progress_tracker import track_progress
 from util import bytes2human, print_message
 from multiprocessing import Process, Queue
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import Dict, List
 from io import BufferedReader
 
 import click
@@ -15,60 +15,71 @@ import sys
 import xxhash
 
 BUFFER_SIZE = 1024 * 1024 * 10  # 10MB
-
+EMPTY_FILE_DIGEST = "EMPTY"
 file_name_queue = Queue()
 bytes_processed_queue = Queue()
 
 
-def build_tree(
-    directory_path: str,
-    directory_hash_map: Dict[str, List[DirectoryNode]],
-    follow_symlinks: bool = False
-) -> Tuple[DirectoryNode, Dict[str, List[DirectoryNode]]]:
-    node = DirectoryNode(path=directory_path)
+def build_metadata_tree(directory_path: str, follow_symlinks: bool = False) -> DirectoryNode:
+    node = DirectoryNode(directory_path)
+
     try:
-        entries = os.scandir(directory_path)
+      entries = os.scandir(directory_path)
     except PermissionError:
         print(f"Could not open directory {directory_path}: permission denied",
               file=sys.stderr)
-        return node, directory_hash_map
+        return node
     for entry in sorted(entries, key=lambda x: x.name):
-        hash_object = xxhash.xxh3_128()
-        file_hash = ""
         try:
-            if entry.is_file() and (follow_symlinks or not entry.is_symlink()):
+            if entry.is_symlink() and not follow_symlinks:
+                continue
+            elif entry.is_file():
                 file_size = entry.stat().st_size
-                # hash the contents of the file, insert into dict
-                with open(entry.path, "rb") as current_file:
-                    reader = BufferedReader(current_file)
-                    while file_chunk := reader.read(BUFFER_SIZE):
-                        hash_object.update(file_chunk)
-                        bytes_processed_queue.put(len(file_chunk))
-                file_hash = hash_object.hexdigest()
-                if (file_size == 0):
-                    file_hash = "EMPTY"  # override prev value, if applicable
-                node.files[entry.path] = FileNode(file_size, file_hash)
-                file_name_queue.put(entry.name)
-            elif entry.is_dir() and not entry.is_symlink():
-                node.subdirectory_nodes[
-                    entry.path], subdirectory_hash_map = build_tree(
-                        entry.path, directory_hash_map, follow_symlinks)
+                node.files[entry.path] = MiNode(entry.name, file_size)
+                node.disk_space += file_size
+                node.num_files += 1
+            elif entry.is_dir():
+                subdir_node = build_metadata_tree(entry.path, follow_symlinks)
+                node.disk_space += subdir_node.disk_space
+                node.num_files += subdir_node.num_files
+                node.num_subdirectories += subdir_node.num_subdirectories + 1
+                node.subdir_nodes[entry.path] = subdir_node
+        except:
+            print(f"Could not read metadata for path {entry.path}", file=sys.stderr)
+    return node
+
+
+def build_hash_map(node: DirectoryNode, working_hash_map: Dict[str, List[DirectoryNode]]) -> str:
+    file_hashes: List[str] = []
+    subdir_hashes: List[str] = []
+    for file_path, file_node in node.files.items():
+        if node.disk_space == 0:
+                file_hashes.append(EMPTY_FILE_DIGEST)
+                # TODO improve handling of empty files (exclude from results by default?)
+                continue
+        hash_builder = xxhash.xxh3_128()
+        try:
+            # hash the contents of the file
+            with open(file_path, "rb") as current_file:
+                file_name_queue.put(file_node.name)
+                reader = BufferedReader(current_file)
+                while file_chunk := reader.read(BUFFER_SIZE):
+                    hash_builder.update(file_chunk)
+                    bytes_processed_queue.put(len(file_chunk))
+            file_hashes.append(hash_builder.hexdigest())
         except (PermissionError, OSError):
-            print(f"Could not open file {entry.path}",
+            print(f"Could not open file {file_path}",
                   file=sys.stderr)
             # use file name & size as stand-in for file contents
-            digest = f"Couldn't Read: {entry.path}".encode("utf-8")
-            file_hash = xxhash.xxh3_128(digest).hexdigest()
-            node.files[entry.path] = FileNode(0, file_hash)
-    if directory_hash_map[node.get_hash()]:
-        # This hash has already been seen. Therefore, subdirectories
-        # are already duplicated in list. Remove immediate children nodes
-        # from hash map, so we only track the roots of duplicate subtrees
-        for child_dir_node in node.subdirectory_nodes.values():
-            directory_hash_map[child_dir_node.get_hash()].remove(
-                child_dir_node)
-    directory_hash_map[node.get_hash()].append(node)
-    return node, directory_hash_map
+            summary = f"Couldn't Read: {file_path}".encode()
+            file_hashes.append(xxhash.xxh3_128_hexdigest(summary))
+    for child_node in node.subdir_nodes.values():
+        subdir_hash = build_hash_map(child_node, working_hash_map)
+        subdir_hashes.append(subdir_hash)
+    node_summary: str = ",".join(sorted(file_hashes) + sorted(subdir_hashes))
+    node.hash = xxhash.xxh3_128_hexdigest(node_summary.encode())
+    working_hash_map[node.hash].append(node)
+    return node.hash
 
 
 def find_duplicate_directory_sets(
@@ -77,9 +88,9 @@ def find_duplicate_directory_sets(
     duplicate_directory_sets = []
     for directory_set in directory_hash_map.values():
         if len(directory_set) > 1:
-            num_files = directory_set[0].get_num_files()
-            num_subdirs = directory_set[0].get_num_subdirectories()
-            disk_space = directory_set[0].get_disk_usage()
+            num_files = directory_set[0].num_files
+            num_subdirs = directory_set[0].num_subdirectories
+            disk_space = directory_set[0].disk_space
             duplicate_directory_sets.append(
                 DuplicateDirectorySet(disk_space, num_files, num_subdirs,
                                       directory_set))
@@ -93,18 +104,20 @@ def find_duplicate_directory_sets(
               default=False,
               help="Follow symbolic links")
 def run(follow_symlinks: bool, directory_path: str) -> None:
+    print("Scanning file metadata...")
+    root_node: DirectoryNode = build_metadata_tree(directory_path=directory_path, follow_symlinks=follow_symlinks)
+    print(f"Found {root_node.num_files} files ({bytes2human(root_node.disk_space)}), {root_node.num_subdirectories} folders")
     p = Process(target=track_progress, args=(file_name_queue, bytes_processed_queue), daemon=True)
     p.start()
-    root_node, directory_hash_map = build_tree(directory_path,
-                                               defaultdict(list),
-                                               follow_symlinks=follow_symlinks)
+    directory_hash_map = defaultdict(list)
+    build_hash_map(root_node, directory_hash_map)
 
     p.terminate()
     p.join()  # block/wait until the process is actually killed
     p.close()  # close any resources associated with the process
 
     # add one to num_subdirectories for root node
-    print_message(f"Scanned {root_node.get_num_subdirectories() + 1} directories, " +
+    print_message(f"Scanned {root_node.num_subdirectories + 1} directories, " +
           f"{len(directory_hash_map)} files ({bytes2human(root_node.disk_space)})",
           file=sys.stderr)
     print()
